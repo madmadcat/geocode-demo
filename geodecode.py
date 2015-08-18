d90d0b282ba245e9c942d2d2edd7897f67a1ab92
madmadcat <madmadcat@gmail.com>
2015-08-18 14:48:46 +0800

    modify utf-8 outout

--- geodecode.py
+++ geodecode.py
@@ -3,6 +3,10 @@
 import simplejson
 import urllib
 import csv
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 GEOCODE_BASE_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
 API_KEY = 'AIzaSyBp3nFBX2qisgdWugfFwvqD_i4MHzR2t24'
@@ -20,13 +24,15 @@ def geocode(address, **geo_args):
     # 以json格式打印经纬度座标
     dict = {}
     dict = result['results'][0]['geometry']['location']
-    dict['locate'] = address
-    print simplejson.dumps(dict, indent=2)
+    dict['locate'] = address.decode('utf-8')
+    for i in dict:
+        print dict[i]
+#    print simplejson.dumps(dict, encoding="utf-8", indent=2)
 
-#if __name__ == '__main__':
-#    geocode(address="内蒙古锡林浩特查干诺尔")
+if __name__ == '__main__':
+    geocode(address='内蒙古锡林浩特查干诺尔')
 
-with open('locate.csv',  'rb') as csvfile:
-    for line in csv.reader(csvfile,  skipinitialspace=True):
-        if __name__  ==  '__main__':
-            geocode(address=line[0])
+#with open('locate.csv',  'rb') as csvfile:
+#    for line in csv.reader(csvfile,  skipinitialspace=True):
+#        if __name__  ==  '__main__':
+#            geocode(address=line[0])
